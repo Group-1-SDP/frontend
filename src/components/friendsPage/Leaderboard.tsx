2e45c0adffa7db38d78bc8b998a38ed6a6a57aa8
matth1eumartin <94123711+matth1eumartin@users.noreply.github.com>
2024-02-26 14:02:58 +0000

    Revert "Restyled Friends Page"
    This reverts commit 1a1a9f4d1507f2feb008dd664d183ccb34cb3a9c.

--- src/components/friendsPage/Leaderboard.tsx
+++ src/components/friendsPage/Leaderboard.tsx
@@ -1,97 +1,33 @@
-import React from "react";
-import LeaderboardElement from "./LeaderboardElement";
-import { motion } from "framer-motion";
+import React from 'react'
+import LeaderboardElement from './LeaderboardElement'
 
 const friends = [
-  {
-    username: "Jonathan",
-    points: getRandomNumber(1, 20),
-    total: getRandomNumber(100, 500),
-    currentUser: true,
-  },
-  {
-    username: "Matthieu",
-    points: getRandomNumber(1, 20),
-    total: getRandomNumber(100, 500),
-    currentUser: false,
-  },
-  {
-    username: "Linus",
-    points: getRandomNumber(1, 20),
-    total: getRandomNumber(100, 500),
-    currentUser: false,
-  },
-  {
-    username: "Dylan",
-    points: getRandomNumber(1, 20),
-    total: getRandomNumber(100, 500),
-    currentUser: false,
-  },
-  // {
-  //   username: "Lewis",
-  //   points: getRandomNumber(1, 20),
-  //   total: getRandomNumber(100, 500),
-  //   currentUser: false,
-  // },
-  // {
-  //   username: "Cem",
-  //   points: getRandomNumber(1, 20),
-  //   total: getRandomNumber(100, 500),
-  //   currentUser: false,
-  // },
-  // {
-  //   username: "Ross",
-  //   points: getRandomNumber(1, 20),
-  //   total: getRandomNumber(100, 500),
-  //   currentUser: false,
-  // },
-  // {
-  //   username: "Lyle",
-  //   points: getRandomNumber(1, 20),
-  //   total: getRandomNumber(100, 500),
-  //   currentUser: false,
-  // },
-  // {
-  //   username: "Pavel",
-  //   points: getRandomNumber(1, 20),
-  //   total: getRandomNumber(100, 500),
-  //   currentUser: false,
-  // },
-];
-
+    { username: 'Jonathan', points: getRandomNumber(1, 20), total: getRandomNumber(100, 500), currentUser: true },
+    { username: 'Matthieu', points: getRandomNumber(1, 20), total: getRandomNumber(100, 500), currentUser: false },
+    { username: 'Linus', points: getRandomNumber(1, 20), total: getRandomNumber(100, 500), currentUser: false },
+    { username: 'Dylan', points: getRandomNumber(1, 20), total: getRandomNumber(100, 500), currentUser: false },
+    { username: 'Lewis', points: getRandomNumber(1, 20), total: getRandomNumber(100, 500), currentUser: false },
+    { username: 'Cem', points: getRandomNumber(1, 20), total: getRandomNumber(100, 500), currentUser: false },
+    { username: 'Ross', points: getRandomNumber(1, 20), total: getRandomNumber(100, 500), currentUser: false },
+    { username: 'Lyle', points: getRandomNumber(1, 20), total: getRandomNumber(100, 500), currentUser: false },
+    { username: 'Pavel', points: getRandomNumber(1, 20), total: getRandomNumber(100, 500), currentUser: false }
+  ];
+  
 function getRandomNumber(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function Leaderboard() {
-  friends.sort((a, b) => b.total - a.total);
+
+    friends.sort((a, b) => b.total - a.total)
 
   return (
-    <div className="flex justify-center ">
-      <div className="w-[400px] border-black border-">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.75 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 0.2,
-            ease: [0, 0.71, 0.2, 1.01],
-          }}
-          className={`p-4 m-4 rounded-md flex items-center justify-between  transition duration-300 bg-gray-200`}
-        >
-          <div className="flex items-center">
-            <p className={`mr-2`}>User</p>
-          </div>
-          <div className="flex items-center">
-            <p className="mr-10">Today</p>
-            <p>Total</p>
-          </div>
-        </motion.div>
-        {friends.map((friend, index) => (
-          <LeaderboardElement key={index} friend={friend} />
-        ))}
-      </div>
+    <div className='mx-20'>
+      {friends.map((friend, index) => (
+        <LeaderboardElement key={index} friend={friend} />
+      ))}
     </div>
-  );
+  )
 }
 
-export default Leaderboard;
+export default Leaderboard
--- src/components/profilePage/ProfileWrapper.tsx
+++ src/components/profilePage/ProfileWrapper.tsx
@@ -1,26 +1,24 @@
-import React from "react";
-import ProfileBox from "./ProfileBox";
-import StatBox from "./StatBox";
+import React from 'react'
+import ProfileBox from './ProfileBox'
+import StatBox from './StatBox'
 
 const profile = {
-  name: "Jonathan",
-  username: "jsteel56",
-  tasksSet: 50,
-  tasksCompleted: 100,
-  total: 1000,
-};
+    name: 'Jonathan',
+    username: 'jsteel56',
+    tasksSet: 50,
+    tasksCompleted: 100,
+    total: 1000
+}
 
 function ProfileWrapper() {
   return (
-    <div className="flex justify-center">
-      <div className="w-[800px] flex justify-between">
+    <div className='mx-20 flex justify-between'>
         <ProfileBox name={profile.name} username={profile.username} />
         <StatBox statName="Tasks Set" stat={profile.tasksSet} />
         <StatBox statName="Tasks Completed" stat={profile.tasksCompleted} />
         <StatBox statName="Total Points" stat={profile.total} />
-      </div>
     </div>
-  );
+  )
 }
 
-export default ProfileWrapper;
+export default ProfileWrapper
