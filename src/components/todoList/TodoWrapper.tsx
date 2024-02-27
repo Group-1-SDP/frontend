00f33c831d7fc2a5e99e839bac632c49b81bf37e
Matthieu <94123711+matth1eumartin@users.noreply.github.com>
2024-02-27 14:51:05 +0000

    Update TodoWrapper.tsx

--- src/components/todoList/TodoWrapper.tsx
+++ src/components/todoList/TodoWrapper.tsx
@@ -1,88 +1,147 @@
 import React, { useEffect, useState } from "react";
 import { TodoForm } from "./TodoForm";
-import { Todo } from "./Todo";
+import { Task, Todo } from "./Todo";
 import { useAtom } from "jotai";
 import { topTodoItem } from "../../App";
 
+function generateRandomString(): string {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < 5; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
 export const TodoWrapper = () => {
-  const [tasks, setTasks] = useState<{ id: number; text: string; completed: boolean; }[]>([]);
+  const [tasks, setTasks] = useState<
+    { id: string; text: string; completed: boolean }[]
+  >([]);
   const [topTask, setTopTask] = useAtom(topTodoItem);
-  const rootAPIEndpoint = 'https://fantastic-broccoli-p6jp6jjvpvwhrj5x-5000.app.github.dev/api/';
+  const APIroot = "http://172.21.12.173:5000/api/";
 
-  // Debug
   useEffect(() => {
-    fetch(rootAPIEndpoint + 'isAlive', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
+    const fetchUserTasks = async () => {
+      try {
+        const response = await fetch(APIroot + "getIncompleteUserTasks", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: "testusername",
+          }),
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch user tasks");
+        }
+        const apiResponse = await response.json();
+        const data = apiResponse.tasks;
+
+        if (Array.isArray(data)) {
+          const formattedTasks = data.map((task: any) => ({
+            id: task.task_id,
+            text: task.contents,
+            completed: task.completed,
+          }));
+          setTasks(formattedTasks);
+        } else {
+          console.error("Data received from server is not an array:", data);
+        }
+      } catch (error) {
+        console.error("Error fetching user tasks:", error);
       }
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      });
-  })
+    };
 
-  useEffect(() => {
-    fetch(rootAPIEndpoint + 'getUserTasks', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: 'test_username'
-      })
-    })
-      .then(response => response.json())
-      .then(data => {
-        // Debug
-        console.log("Data from getUserTasks: ", data);
-        const formattedTasks = data.map((task: any) => ({
-          id: task.task_id,
-          text: task.contents,
-          completed: task.completed
-        }));
-        setTasks(formattedTasks);
-      });
-  })
-  
+    fetchUserTasks();
+  }, []);
+
   useEffect(() => {
     if (tasks.length > 0) {
-      const newTopTask = tasks[0].text
-      setTopTask(newTopTask)
+      const newTopTask = tasks[0].text;
+      setTopTask(newTopTask);
     }
-    
-    
-  }, [tasks])
+  }, [tasks]);
 
   const addTask = (text: string) => {
+    const task_id = generateRandomString();
     const newTask = {
-      id: Date.now(),
+      id: task_id,
       text,
       completed: false,
     };
+    sendToAPI(task_id, text)
     setTasks([...tasks, newTask]);
+   
   };
 
-  const deleteTask = (id: number) => {
-    setTasks(tasks.filter(task => task.id !== id));
-  }
-
-  const toggleCompleted = (id: number) => {
-    setTasks(tasks.map(task => {
-      if (task.id === id) {
-        return {...task, completed: !task.completed};
-      } else {
-        return task;
+  const sendToAPI = async (task_id: string, contents: string) => {
+    try {
+      const response = await fetch(APIroot + "addTask", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "testusername",
+          task_id: task_id,
+          contents: contents,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to fetch user tasks");
       }
-    }));
-  }
+    } catch (error) {
+      console.error("error adding task");
+    }
+  };
 
-  const sortedTasks = tasks.sort((a, b) => (a.completed === b.completed ? 0 : a.completed ? 1 : -1));
+  const updateAPI = async (task_id: string) => {
+    console.log("updating api")
+    try {
+      const response = await fetch(APIroot + "updateTask", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "testusername",
+          task_id: task_id,
+          completed: "true"
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to update user task");
+      }
+    } catch (error) {
+      console.error("error updating task");
+    }
+  };
+
+  const deleteTask = (id: string) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleCompleted = (id: string) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: !task.completed };
+        } else {
+          return task;
+        }
+      })
+    );
+    updateAPI(id);
+  };
+
+  const sortedTasks = tasks.sort((a, b) =>
+    a.completed === b.completed ? 0 : a.completed ? 1 : -1
+  );
 
   return (
-    <div className="mx-20">
-      
+    <div className="flex justify-center">
+    <div className="w-[600px]">
       <TodoForm addTask={addTask} />
       {sortedTasks.map((task) => (
         <Todo
@@ -92,6 +151,7 @@ export const TodoWrapper = () => {
           toggleCompleted={toggleCompleted}
         />
       ))}
+    </div>
     </div>
   );
 };
