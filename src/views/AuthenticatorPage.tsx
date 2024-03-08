21cc9a3c7aa6bd3c1b46c7566afb148b36462d85
LinusTC <tongchun03@gmail.com>
2024-03-08 23:40:36 +0000

    m

--- src/views/AuthenticatorPage.tsx
+++ src/views/AuthenticatorPage.tsx
@@ -10,9 +10,7 @@ const AuthenticatorPage = () => {
     <>
       <div className="relative z-10 pointer-events-none">
         <Icon />
-        <div className="flex justify-center">
-          <Authenticator />
-        </div>
+        <Authenticator />
       </div>
       <canvas
         ref={canvasRef}
