b1659a8a30b2c472545b37298fd6912f0fc0985d
LinusTC <tongchun03@gmail.com>
2024-03-11 01:42:43 +0000

    topbar updates

--- src/components/Utils/Navigation/TopBar.tsx
+++ src/components/Utils/Navigation/TopBar.tsx
@@ -1,13 +1,17 @@
 import { usernameAtom } from "../GlobalState";
 import { useAtom } from "jotai";
+import { CiCircleChevDown } from "react-icons/ci";
 
 function topBar() {
   const [username] = useAtom(usernameAtom);
   return (
     <div className="flex justify-end pr-48 pt-3 w-full h-16">
-      <div className="flex items-center">
-        <div className="p-5 mr-2 rounded-full bg-black"></div>
-        <h1>{username}</h1>
+      <div className="flex items-center space-x-4">
+        <img className="rounded-full" src="https://placehold.co/55x55" />
+        <div className="flex flex-col font-semibold">
+          <h1>{username}</h1> <h1>level 0</h1>
+        </div>
+        <CiCircleChevDown size={30} />
       </div>
     </div>
   );
