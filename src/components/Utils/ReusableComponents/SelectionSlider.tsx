cbc4da4495901ed1e3c363682ed4aa94998dab2e
linusTC <tongchun03@gmail.com>
2024-03-20 12:45:54 +0000

    small fix for selectionslider, how did matthieu not fix this sooner

--- src/components/Utils/ReusableComponents/SelectionSlider.tsx
+++ src/components/Utils/ReusableComponents/SelectionSlider.tsx
@@ -10,7 +10,9 @@ type Tab = {
 function SelectionSlider(props: { tabs: Tab[] }) {
   const tabs = props.tabs;
 
-  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+  const [selectedTabName, setSelectedTabName] = useState(tabs[0].name);
+  const selectedTab = tabs.find((tab) => tab.name === selectedTabName);
+
   return (
     <div className="w-full h-full rounded-xl">
       <nav className="">
@@ -18,28 +20,35 @@ function SelectionSlider(props: { tabs: Tab[] }) {
           {tabs.map((item) => (
             <li
               key={item.name}
-              className={item === selectedTab ? "selected" : ""}
-              onClick={() => setSelectedTab(item)}
+              className={`cursor-pointer ${
+                item.name === selectedTabName ? "font-bold" : "font-medium"
+              }`}
+              onClick={() => setSelectedTabName(item.name)}
             >
               {`${item.name}`}
-              {item === selectedTab ? (
-                <motion.div className="underline" layoutId="underline" />
-              ) : null}
+              {item.name === selectedTabName && (
+                <motion.div
+                  className="underline bg-black w-full"
+                  layoutId="underline"
+                />
+              )}
             </li>
           ))}
         </ul>
       </nav>
       <main className="py-5">
         <AnimatePresence mode="wait">
-          <motion.div
-            key={selectedTab ? selectedTab.name : "empty"}
-            initial={{ y: 10, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: -10, opacity: 0 }}
-            transition={{ duration: 0.2 }}
-          >
-            {selectedTab.page}
-          </motion.div>
+          {selectedTab && (
+            <motion.div
+              key={selectedTab.name}
+              initial={{ y: 10, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -10, opacity: 0 }}
+              transition={{ duration: 0.2 }}
+            >
+              {selectedTab.page}
+            </motion.div>
+          )}
         </AnimatePresence>
       </main>
     </div>
