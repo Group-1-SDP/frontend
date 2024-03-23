40ce4246fd95a0dade17e7f16ee9a7334b8f3635
Jonathan-Steel <j.p.l.steel@sms.ed.ac.uk>
2024-03-23 16:14:15 +0000

    Fixed the Color Input

--- src/components/schedulePage/ScheduleForm.tsx
+++ src/components/schedulePage/ScheduleForm.tsx
@@ -4,34 +4,37 @@ interface ScheduleFormProps {
   addEvent: (name: string, slot: string, color: string) => void;
 }
 
-const colorDefault = {
-  greenAccent: false,
-  yellowAccent: false,
-  blueAccent: false,
-  redAccent: false,
-  blackAccent: false,
-  greyAccent: false,
-};
+// const colorDefault = {
+//   greenAccent: false,
+//   yellowAccent: false,
+//   blueAccent: false,
+//   redAccent: false,
+//   blackAccent: false,
+//   greyAccent: false,
+// };
 
 function ScheduleForm({ addEvent }: ScheduleFormProps) {
   const [name, setName] = useState("");
   const [hours, setHours] = useState("");
-  const [color, setColor] = useState<{ [key: string]: boolean }>(colorDefault);
+  const [color, setColor] = useState("project-1");
 
-  const getSelectedColor = (): string | undefined => {
-    const selectedColor = Object.keys(color).find((c) => color[c]);
-    return selectedColor;
-  };
+  //   const [color, setColor] = useState<{ [key: string]: boolean }>(colorDefault);
 
-  const selectColor = (colorValue: string) => {
-    if (!color.hasOwnProperty(colorValue)) {
-      return;
-    }
-    const updatedColor = Object.fromEntries(
-      Object.entries(color).map(([key]) => [key, key === colorValue])
-    );
-    setColor(updatedColor);
-  };
+  //   const getSelectedColor = (): string => {
+  //     let selectedColor = Object.keys(color).find((c) => color[c]);
+  //     if (selectedColor === undefined) {selectedColor = "greyAccent"}
+  //     return selectedColor;
+  //   };
+
+  //   const selectColor = (colorValue: string) => {
+  //     if (!color.hasOwnProperty(colorValue)) {
+  //       return;
+  //     }
+  //     const updatedColor = Object.fromEntries(
+  //       Object.entries(color).map(([key]) => [key, key === colorValue])
+  //     );
+  //     setColor(updatedColor);
+  //   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -42,13 +45,11 @@ function ScheduleForm({ addEvent }: ScheduleFormProps) {
     if (isNaN(hoursNumber) || hoursNumber < 0 || hoursNumber > 23) {
       return;
     }
-    if (getSelectedColor === undefined) {
-    }
     console.log(`Adding Event "${name}", "${hours}:00", "${color}"...`);
-    addEvent(name, `${hours}:00`, getSelectedColor());
+    addEvent(name, `${hours}:00`, color);
     setName("");
     setHours("");
-    setColor(colorDefault);
+    setColor("project-1");
   };
 
   return (
@@ -76,17 +77,25 @@ function ScheduleForm({ addEvent }: ScheduleFormProps) {
           />
           :<p className="w-6 text-center ml-1">00</p>
         </div>
-        <div className="flex flex-row">
+        <select value={color} onChange={(e) => setColor(e.target.value)} className={`my-2`}>
+          <option value="break">Grey</option>
+          <option value="project-1">Green</option>
+          <option value="project-2">Yellow</option>
+          <option value="project-3">Blue</option>
+          <option value="project-4">Red</option>
+          <option value="project-5">Black</option>
+        </select>
+        {/* <div className="flex flex-row">
             {Object.keys(color).map((colorValue) => (
             <input
                 type="checkbox"
                 id={colorValue}
                 checked={color[colorValue]}
                 onChange={() => selectColor(colorValue)}
-                className={`my-2 mr-2 p-2 checked:bg-green-600`}
+                className={`my-2 mr-2 p-2 outline-green-400 checked:bg-green-600`}
             />
             ))}
-        </div>
+        </div> */}
         <button
           type="submit"
           className="p-2 bg-greenAccent text-white font-bold rounded-md hover:bg-green-700"
